59133739061e4c863fd8576a06a72a1a30dacec0
Migz <juanmiguelganfernandez@gmail.com>
2023-10-07 20:16:49 +0800

    Update resource for fetch methods

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
 
-          fetch('http://localhost:3000/imageurl', {
+          fetch('https://smart-brain-api-vbh8.onrender.com/imageurl', {
                   method: 'post',
                   headers: {'Content-Type': 'application/json'},
                   body: JSON.stringify({
@@ -73,7 +73,7 @@ class App extends Component {
           })
           .then(response => {
             if (response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://smart-brain-api-vbh8.onrender.com/image', {
                   method: 'put',
                   headers: {'Content-Type': 'application/json'},
                   body: JSON.stringify({
